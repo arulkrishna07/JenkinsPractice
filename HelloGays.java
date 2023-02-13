ad5f2765950437456613dadd9874d984ef2282d4
arulkrishna07 <118039233+arulkrishna07@users.noreply.github.com>
2023-02-13 11:26:48 +0530

    First commit

---
+++ HelloGays.java
@@ -0,0 +1,12 @@
+public class HelloGays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+		System.out.println("Hi Arul");
+		System.out.println("Hi Bala");
+		System.out.println("Hi Chilip");
+
+	}
+
+}
